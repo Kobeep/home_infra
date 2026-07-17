7e21d4e0496c285057399c2eae4904f646d54c2c
Kobeep <kuba.pospieszny@gmail.com>
2026-07-17 14:40:21 +0200

    small fix -> changed indents

--- lib/Constants.py
+++ lib/Constants.py
@@ -18,14 +18,11 @@ git_node_mount_path = Path("/tmp/git")
 
 # Cronjob schedules
 update_os_cronjob_schedule = "0 2 * * *"
-update_os_cronjob_command = f"{update_os_cronjob_schedule} /usr/bin/python3 {bin_path / 'update_os.py'} >> {log_path}/system_log_update_os_$(date +\%Y\%m\%d).log 2>&1"
-
 clean_orphaned_logs_cronjob_schedule = "0 3 * * *"
-clean_orphaned_logs_cronjob_command = f"{clean_orphaned_logs_cronjob_schedule} /usr/bin/python3 {bin_path / 'clean_orphaned_logs.py'} >> {log_path}/system_log_clean_orphaned_logs_$(date +\%Y\%m\%d).log 2>&1"
-
 github_cronjob_schedule = "0 4 * * *"
-github_cronjob_command = f"{github_cronjob_schedule} /usr/bin/python3 {bin_path / 'github.py'} >> {log_path}/system_log_github_$(date +\%Y\%m\%d).log 2>&1"
-
+update_os_cronjob_command = rf"{update_os_cronjob_schedule} /usr/bin/python3 {bin_path / 'update_os.py'} >> {log_path}/system_log_update_os_\$(date +\%Y\%m\%d).log 2>&1"
+clean_orphaned_logs_cronjob_command = rf"{clean_orphaned_logs_cronjob_schedule} /usr/bin/python3 {bin_path / 'clean_orphaned_logs.py'} >> {log_path}/system_log_clean_orphaned_logs_\$(date +\%Y\%m\%d).log 2>&1"
+github_cronjob_command = rf"{github_cronjob_schedule} /usr/bin/python3 {bin_path / 'github.py'} >> {log_path}/system_log_github_\$(date +\%Y\%m\%d).log 2>&1"
 ###################################################################################################################
 list_of_cronjobs_to_apply = [update_os_cronjob_command, clean_orphaned_logs_cronjob_command, github_cronjob_command]
 ###################################################################################################################
